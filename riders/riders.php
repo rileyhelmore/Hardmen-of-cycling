72f8061e2db57fbc2ed72095c47fd7566f513f4d
Riley Helmore <rileyhelmore@gmail.com>
2020-09-12 17:47:47 +1000

    Learning some javscript
    Javascript was used with the feature that inlarges images when mouse hovers over them.

---
+++ riders/riders.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<link rel="stylesheet" href="./riders_style.css" type="text/css">
+</head>
+<body>
+  <h1 class="h1">Hardmen of cycling</h1>
+  <h2 class="h2">Riders</h2>
+  <ul class="ul">
+    <li class="li"><a href="/hardmen_of_cycling/index.html">Home</a></li>
+    <li class="li"><a href="./stories.php">Stories</a></li>
+    <li class="li"><a href="./events.php">Events</a></li>
+    <li class="li"><a href="./TDF.php">TDF</a></li>
+  </ul>
+
+
+
+
+</body>
+</html>
